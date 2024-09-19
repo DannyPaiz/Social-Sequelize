ac292b0989fd1b73dd52bc7ca994b004cbb2fb9d
danny paiz <daniel.martinez.paiz@verizon.com>
2024-09-19 10:43:05 -0400

    connection.js task completed

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,16 @@
+const path = require('path');
+const { Sequelize, Model } = require('sequelize');
 
+const sequelize = new Sequelize(
+    {
+        dialect: 'sqlite',
+        storage: path.join(__dirname, 'sequelize.sqlite')
+    }
+);
 
-
-
+sequelize.authenticate();
 
 module.exports = {
-    db,
+    sequelize,
     
 }
